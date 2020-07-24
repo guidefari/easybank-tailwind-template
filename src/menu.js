e0d8ec1cb8a0936c3d1a9d457ea877bbd0bb2e8a
GT Fari <guideg6@gmail.com>
2020-07-24 22:47:49 +0200

    navbar logic start

--- src/menu.js
+++ src/menu.js
@@ -2,23 +2,31 @@ document.getElementById("menuBtn").addEventListener("click", navToggle);
 
 
 var nav = document.getElementById('site-menu');
-    var header = document.getElementById('top');
+var header = document.getElementById('top');
 
-    window.addEventListener('scroll', function() {
-      if (window.scrollY >=400) { // adjust this value based on site structure and header image height
-        nav.classList.add('nav-sticky');
-        header.classList.add('pt-scroll');
-      } else {
-        nav.classList.remove('nav-sticky');
-        header.classList.remove('pt-scroll');
-      }
-    });
+window.addEventListener('scroll', function() {
+  if (window.scrollY >=400) { // adjust this value based on site structure and header image height
+    nav.classList.add('nav-sticky');
+    header.classList.add('pt-scroll');
+  } else {
+    nav.classList.remove('nav-sticky');
+    header.classList.remove('pt-scroll');
+  }
+});
 
-    function navToggle() {
-        var btn = document.getElementById('menuBtn');
-        var nav = document.getElementById('menu');
+function navToggle() {
+    var btn = document.getElementById('menuBtn');
+    var nav = document.getElementById('menu');
 
-        btn.classList.toggle('open');
-        nav.classList.toggle('closed');
-        nav.classList.toggle('tray-open');
-    }
+    btn.classList.toggle('open');
+    nav.classList.toggle('closed');
+    nav.addEventListener('transitionend', changedp);
+    nav.classList.toggle('tray-open');
+}
+
+function changedp() {
+  var nav = document.getElementById('menu');
+  if(nav.classList.contains('closed'))
+{  nav.classList.toggle('hidden');
+}  
+}
